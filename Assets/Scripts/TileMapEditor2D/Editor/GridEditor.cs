e5877f3ff1d6ba9d53b06f15623c1aead17eebd6
Simon Bergqvist <bergqvist.simon@gmail.com>
2014-04-28 14:27:29 +0200

    first

---
+++ Assets/Scripts/TileMapEditor2D/Editor/GridEditor.cs
@@ -0,0 +1,50 @@
+﻿using UnityEngine;
+using UnityEditor;
+using System.Collections;
+
+[CustomEditor (typeof(Grid))]
+public class GridEditor : Editor {
+	Grid grid;
+
+	Texture2D texture;
+
+	public void OnEnable()
+	{
+		grid = (Grid)target;
+	}
+
+	public override void OnInspectorGUI()
+	{
+		GUILayout.BeginHorizontal();
+		GUILayout.Label("Tile Width ");
+		grid.width = EditorGUILayout.FloatField(grid.width, GUILayout.Width(50));
+		GUILayout.EndHorizontal();
+		
+		GUILayout.BeginHorizontal();
+		GUILayout.Label("Tile Height ");
+		grid.height = EditorGUILayout.FloatField(grid.height, GUILayout.Width(50));
+		GUILayout.EndHorizontal();
+
+		GUILayout.BeginHorizontal ();
+		GUILayout.Label(" Visible ");
+		grid.visible = EditorGUILayout.Toggle (grid.visible);
+		GUILayout.EndHorizontal ();
+
+		GUILayout.BeginHorizontal ();
+		texture = (Texture2D) EditorGUILayout.ObjectField(" Texture ", texture, typeof (Texture2D), false);
+		GUILayout.EndHorizontal ();
+
+		Rect posRect = new Rect (40, 300, 100, 100);
+		float textureSize = texture.width / grid.width * 100;
+		Rect randomRect = new Rect(0,0,textureSize,1);
+
+		GUILayout.BeginHorizontal ();
+		for(int i=0; i<textureSize;i++){
+			GUI.DrawTextureWithTexCoords (posRect, texture, randomRect);
+		}
+		
+		GUILayout.EndHorizontal ();
+
+		SceneView.RepaintAll();
+	}
+}
---
+++ Assets/Scripts/TileMapEditor2D/Grid.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Grid : MonoBehaviour {
+
+	public float width = 0.32f;
+	public float height = 0.32f;
+	public bool visible = true;
+
+	void OnDrawGizmos(){
+		if (width < 0.1 || height < 0.1) {
+				visible = false;
+		}
+		Vector3 pos = Camera.current.transform.position;
+		if (visible) {
+						for (float y = pos.y - 400.0f; y < pos.y + 400.0f; y+= height) {
+								Gizmos.DrawLine (new Vector3 (-1000000.0f, Mathf.Floor (y / height) * height, 0.0f),
+			                new Vector3 (1000000.0f, Mathf.Floor (y / height) * height, 0.0f));
+						}
+		
+						for (float x = pos.x - 600.0f; x < pos.x + 600.0f; x+= width) {
+								Gizmos.DrawLine (new Vector3 (Mathf.Floor (x / width) * width, -1000000.0f, 0.0f),
+			                new Vector3 (Mathf.Floor (x / width) * width, 1000000.0f, 0.0f));
+						}
+				}
+	}
+}
