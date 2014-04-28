7ba1e506a80df780ef01c38410eb7ade63abf074
tEFFx <erikkoii@gmail.com>
2014-04-28 17:55:34 +0200

    grid shows up

---
+++ Assets/Scripts/TileMapEditor2D/Editor/TileEditor.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using UnityEditor;
+
+public class TileEditor : EditorWindow {
+	private bool showGrid = true;
+
+	static private Grid grid;
+	static private GameObject gridGameObject;
+
+	[MenuItem("Edit/TileEditor")]
+	static void Init()
+	{
+		var window = (TileEditor)EditorWindow.GetWindow(typeof(TileEditor));
+		window.maxSize = new Vector2 (200, 100);
+		gridGameObject = new GameObject ("GridObject");
+		gridGameObject.AddComponent("Grid");
+	}
+
+	void OnGUI()
+	{
+		showGrid = EditorGUILayout.Toggle ("Show grid", showGrid);
+	}
+
+	void OnDestroy()
+	{
+		GameObject.DestroyImmediate (gridGameObject);
+	}
+}
