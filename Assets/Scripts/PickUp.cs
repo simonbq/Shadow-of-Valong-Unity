6c8910525ff9d09fea5bcf3520189495d45b35f0
Simon Bergqvist <bergqvist.simon@gmail.com>
2014-04-29 11:57:28 +0200

    Forgotten files

---
+++ Assets/Scripts/PickUp.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PickUp : MonoBehaviour {
+
+	Transform originalParent;
+
+	// Use this for initialization
+	void Start () {
+		if (transform.parent != null) {
+			originalParent = transform.parent;
+		}
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+	void Interact (Transform userTransform) {
+		if (transform.parent != userTransform) {
+			transform.parent = userTransform;
+			transform.position = new Vector3(userTransform.position.x, userTransform.position.y+renderer.bounds.size.y, 0);
+		}else{
+			transform.parent = originalParent;
+			transform.position = new Vector3(userTransform.position.x, userTransform.position.y-renderer.bounds.size.y, 0);
+		}
+	}
+}
