c0ebfc534b4f3255b68819f053653a5e9be560fb
simonbq <bergqvist.simon@gmail.com>
2014-05-07 12:59:56 +0200

    Dialogue gui & internal id replacement

--- Assets/Scripts/Controllers/GameController.cs
+++ Assets/Scripts/Controllers/GameController.cs
@@ -2,14 +2,20 @@
 using System.Collections;
 public class GameController : MonoBehaviour {
 
-    private DialogueContainer dialogues;
-    private static bool inDialogue = false;
-    private static int currentDialogue;
-    private static int currentText;
-    private static bool justStartedDialogue;
 
+	//Gamestate
     public enum GameState { GAME, PAUSED, MAINMENU, MAP };
     public static GameState gameState = GameState.GAME;
+    
+    //Public variables accessible through editor
+    public Texture2D dialogueTexture;
+    
+    //Private variables
+	private DialogueContainer dialogues;
+	private static bool inDialogue = false;
+	private static int currentDialogue;
+	private static int currentText;
+	private static bool justStartedDialogue;
 
 	// Use this for initialization
 	void Start (){
@@ -36,10 +42,12 @@ public class GameController : MonoBehaviour {
             //Debug.Log(dialogues.Dialogues[currentDialogue].Texts.Count);
     		if(currentText < dialogues.Dialogues[currentDialogue].Texts.Count){
                 string dialogueText = replaceVariables(dialogues.Dialogues[currentDialogue].Texts[currentText].value);
-                //string dialogueText = "something";
-                string speakerId = string.Empty + dialogues.Dialogues[currentDialogue].Texts[currentText].SpeakerId;
-                //string speakerId = "someone";
-				GUI.Label((new Rect(400, 100, 400, 20)),  speakerId + " says: " + dialogueText);
+
+                string speakerId = replaceSpeakerId(dialogues.Dialogues[currentDialogue].Texts[currentText].SpeakerId);
+
+				GUI.Label((new Rect((Screen.width/2)-(dialogueTexture.width/2), Screen.height-dialogueTexture.height-100, dialogueTexture.width, dialogueTexture.height)),  dialogueTexture);
+				GUI.Label((new Rect((Screen.width/2)-(dialogueTexture.width/2)+100, Screen.height-dialogueTexture.height-20, dialogueTexture.width, dialogueTexture.height)), speakerId + " says: ");
+				GUI.Label((new Rect((Screen.width/2)-(dialogueTexture.width/2)+100, Screen.height-dialogueTexture.height, dialogueTexture.width, dialogueTexture.height)), dialogueText);
 			}else{
                 inDialogue = false;
                 gameState = GameState.GAME;
@@ -58,10 +66,24 @@ public class GameController : MonoBehaviour {
 
     string replaceVariables(string dialogueText){
         if (dialogueText != null){
-            string newText = dialogueText.Replace("{PlayerName}", "Kunkka");
+            string newText = dialogueText.Replace("{PlayerName}", "Link");
             return newText;
         }else{
             return dialogueText;
         }
+    }
+    
+    string replaceSpeakerId(int id){
+    	string oldName = string.Empty + id;
+    	string name = oldName.Replace("0", "Link");
+		name = name.Replace("1", "Gertrude");
+		name = name.Replace("2", "Calle");
+		name = name.Replace("3", "Dog");
+		
+		if(oldName.Equals(name, System.StringComparison.Ordinal)){
+    		return string.Empty + "NPC_ID_" + id;
+    	}else{
+    		return name;
+    	}
     }
 }
