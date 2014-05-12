adc6ca72f877e99f5d3dca55ee1b9fd028e36ce7
Simon Bergqvist <bergqvist.simon@gmail.com>
2014-05-12 15:00:04 +0200

    Fixed line endings

--- Assets/Scripts/Controllers/DialogueController.cs
+++ Assets/Scripts/Controllers/DialogueController.cs
@@ -20,25 +20,19 @@ public class DialogueController : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-        if (inDialogue && !justStartedDialogue && Input.GetButtonDown("Interact"))
-        {
+        if (inDialogue && !justStartedDialogue && Input.GetButtonDown("Interact")){
             currentText++;
         }
-        if (inDialogue)
-        {
+        if (inDialogue){
             GameController.gameState = GameController.GameState.PAUSED;
         }
 
         justStartedDialogue = false;
 	}
 
-    void OnGUI()
-    {
-        if (inDialogue)
-        {
-            //Debug.Log(dialogues.Dialogues[currentDialogue].Texts.Count);
-            if (currentText < dialogues.Dialogues[currentDialogue].Texts.Count)
-            {
+    void OnGUI(){
+        if (inDialogue){
+            if (currentText < dialogues.Dialogues[currentDialogue].Texts.Count){
                 string dialogueText = replaceVariables(dialogues.getDialogue(currentDialogue).getText(currentText));
 
                 string speakerId = replaceSpeakerId(dialogues.getDialogue(currentDialogue).getSpeakerId(currentText));
@@ -46,17 +40,14 @@ public class DialogueController : MonoBehaviour {
                 GUI.Label((new Rect((Screen.width / 2) - (dialogueTexture.width / 2), Screen.height - dialogueTexture.height - 100, dialogueTexture.width, dialogueTexture.height)), dialogueTexture);
                 GUI.Label((new Rect((Screen.width / 2) - (dialogueTexture.width / 2) + 100, Screen.height - dialogueTexture.height - 20, dialogueTexture.width, dialogueTexture.height)), speakerId + " says: ");
                 GUI.Label((new Rect((Screen.width / 2) - (dialogueTexture.width / 2) + 100, Screen.height - dialogueTexture.height, dialogueTexture.width, dialogueTexture.height)), dialogueText);
-            }
-            else
-            {
+            }else{
                 inDialogue = false;
                 GameController.gameState = GameController.GameState.GAME;
             }
         }
     }
 
-    public static void startDialogue(int dialogueId)
-    {
+    public static void startDialogue(int dialogueId){
         Debug.Log("Dialogue started: " + dialogueId);
         currentDialogue = dialogueId;
         currentText = 0;
@@ -64,33 +55,25 @@ public class DialogueController : MonoBehaviour {
         inDialogue = true;
     }
 
-    string replaceVariables(string dialogueText)
-    {
-        if (dialogueText != null)
-        {
+    string replaceVariables(string dialogueText){
+        if (dialogueText != null){
             string newText = dialogueText.Replace("{PlayerName}", "Link");
             return newText;
-        }
-        else
-        {
+        }else{
             return dialogueText;
         }
     }
 
-    public static string replaceSpeakerId(int id)
-    {
+    public static string replaceSpeakerId(int id){
         string oldName = string.Empty + id;
         string name = oldName.Replace("0", "Link");
         name = name.Replace("1", "Gertrude");
         name = name.Replace("2", "Calle");
         name = name.Replace("3", "Dog");
 
-        if (oldName.Equals(name, System.StringComparison.Ordinal))
-        {
+        if (oldName.Equals(name, System.StringComparison.Ordinal)){
             return string.Empty + "NPC_ID_" + id;
-        }
-        else
-        {
+        }else{
             return name;
         }
     }
