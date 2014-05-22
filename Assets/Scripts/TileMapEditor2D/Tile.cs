5af17d77368d17be3da534cce2cc0a63ee831a1a
Simon Bergqvist <bergqvist.simon@gmail.com>
2014-05-22 16:26:36 +0200

    Fixed performance issues in Tile.cs caused by excessive use of getcomponent

--- Assets/Scripts/TileMapEditor2D/Tile.cs
+++ Assets/Scripts/TileMapEditor2D/Tile.cs
@@ -1,17 +1,27 @@
 ﻿using UnityEngine;
 using System.Collections;
+using System.Collections.Generic;
 
 public class Tile : MonoBehaviour {
 	public int tileID;
 	public bool locked = false;
+    
+    BoxCollider2D c;
+    private bool hasGottenCollider = false;
 
 	void OnDrawGizmos()
-	{	
-		BoxCollider2D c = GetComponent<BoxCollider2D> ();
+	{
+        if(!hasGottenCollider){
+            c = GetComponent<BoxCollider2D>();
+            hasGottenCollider = true;
+        }
+
 		if(c != null)
 		{
+            
 			Gizmos.color = Color.red;
 			Vector3 pos = transform.position;
+            
 			Gizmos.DrawLine(new Vector3(pos.x, pos.y, 1f),
 			                new Vector3(pos.x+c.size.x, pos.y, 1f));
 			Gizmos.DrawLine(new Vector3(pos.x, pos.y, 1f),
@@ -21,5 +31,6 @@ public class Tile : MonoBehaviour {
 			Gizmos.DrawLine(new Vector3(pos.x+c.size.x, pos.y, 1f),
 			                new Vector3(pos.x+c.size.x, pos.y-c.size.y, 1f));
 		}
+        
 	}
 }
