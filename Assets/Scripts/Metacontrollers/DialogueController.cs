0cf9e6b0dda5e057353ea92aad3ca9f1fa8be82e
Simon Bergqvist <bergqvist.simon@gmail.com>
2014-05-28 15:29:56 +0200

    reworking dialogue gui

--- Assets/Scripts/Metacontrollers/DialogueController.cs
+++ Assets/Scripts/Metacontrollers/DialogueController.cs
@@ -5,6 +5,7 @@ public class DialogueController : MonoBehaviour {
 
     //Public variables accessible through editor
     public Texture2D dialogueTexture;
+    public GUISkin mSkin;
 
     //Private variables
     private DialogueContainer dialogues = new DialogueContainer();
@@ -16,6 +17,7 @@ public class DialogueController : MonoBehaviour {
 	// Use this for initialization
 	void Start () {
         dialogues = DialogueContainer.Load("dialogue.xml");
+        
 	}
 	
 	// Update is called once per frame
@@ -31,20 +33,34 @@ public class DialogueController : MonoBehaviour {
 	}
 
     void OnGUI(){
+        
+
         if (inDialogue){
             if (currentText < dialogues.Dialogues[currentDialogue].Texts.Count){
                 string dialogueText = replaceVariables(dialogues.getDialogue(currentDialogue).getText(currentText));
 
                 string speakerId = replaceSpeakerId(dialogues.getDialogue(currentDialogue).getSpeakerId(currentText));
 
-                GUI.Label((new Rect((Screen.width / 2) - (dialogueTexture.width / 2), Screen.height - dialogueTexture.height - 100, dialogueTexture.width, dialogueTexture.height)), dialogueTexture);
-                GUI.Label((new Rect((Screen.width / 2) - (dialogueTexture.width / 2), Screen.height - dialogueTexture.height - 20, dialogueTexture.width, dialogueTexture.height)), speakerId + " says: ");
-                GUI.Label((new Rect((Screen.width / 2) - (dialogueTexture.width / 2), Screen.height - dialogueTexture.height, dialogueTexture.width, dialogueTexture.height)), dialogueText);
+                GUI.Label(new Rect((Screen.width / 2) - getWidth(dialogueTexture.width)/2, Screen.height - getHeight(dialogueTexture.height + 80), getWidth(dialogueTexture.width), getHeight(dialogueTexture.height)), dialogueTexture);
+                GUI.skin = mSkin;
+                GUI.skin.label.fontSize = Mathf.RoundToInt(getHeight(30));
+
+                GUI.Label(new Rect(Screen.width/2 - getWidth(dialogueTexture.width - 50)/2, Screen.height - getHeight(dialogueTexture.height + 35), getWidth(dialogueTexture.width - 100), getHeight(dialogueTexture.height)), "<color=black>"+speakerId + " says: "+"</color>");
+                GUI.skin.label.fontSize = Mathf.RoundToInt(getHeight(40));
+                GUI.Label(new Rect(Screen.width / 2 - getWidth(dialogueTexture.width - 30) / 2, Screen.height - getHeight(dialogueTexture.height), getWidth(dialogueTexture.width-100), getHeight(dialogueTexture.height)), "<color=black>"+dialogueText+"</color>");
             }else{
                 inDialogue = false;
                 GameController.gameState = GameController.GameState.GAME;
             }
         }
+    }
+    float getHeight(float height)
+    {
+        return height * Screen.height / 1080;
+    }
+    float getWidth(float width)
+    {
+        return width * Screen.width / 1920;
     }
 
     public static void startDialogue(int dialogueId){
