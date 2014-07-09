0917cac2c2752e5f116a20208a42d3155293e434
tEFFx <erikkoii@gmail.com>
2014-07-09 18:39:44 +0200

    Started working on navmeshbased pathfinding

---
+++ Assets/Scripts/NavMesh2D/Editor/NavMesh2DTerrainGenerator.cs
@@ -0,0 +1,102 @@
+﻿using UnityEngine;
+using UnityEditor;
+using System.Collections;
+using System.Collections.Generic;
+
+public class NavMesh2DTerrainGenerator : EditorWindow {
+
+    private int terrainResolution = 64;
+    private float tileSize = 0.32f;
+    private GameObject terrain;
+	// Use this for initialization
+    [MenuItem("Edit/2D Navigation")]
+
+    static void Init()
+    {
+        var window = (NavMesh2DTerrainGenerator)EditorWindow.GetWindow(typeof(NavMesh2DTerrainGenerator));
+        window.minSize = new Vector2(240, 480);
+        window.title = "2D Navigation";
+    }
+
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    void OnGUI()
+    {
+        terrainResolution = EditorGUILayout.IntField("Terrain resolution", terrainResolution);  
+        GUILayout.Label("Tile size: " + tileSize);
+        GUILayout.Label("Terrain size: " + terrainResolution / tileSize);
+        
+        if (GUILayout.Button("Generate navigation terrain"))
+        {
+            terrain = generateNavTerrain();
+        }
+    }
+
+    private GameObject generateNavTerrain()
+    {
+        GameObject tiles = GameObject.Find("Tiles");
+        List<Rect> positions = new List<Rect>();
+        if (tiles != null)
+        {
+            foreach (Transform layer in tiles.transform)
+            {
+                foreach(Transform tile in layer.transform)
+                {
+                    if (tile.gameObject.layer == 11)
+                    {
+                        SpriteRenderer spr = tile.transform.gameObject.GetComponent<SpriteRenderer>();
+
+                        Rect t = new Rect();
+                        t.x = -tile.transform.position.y;
+                        t.y = tile.transform.position.x;
+                        t.width = spr.bounds.size.x;
+                        t.height = spr.bounds.size.y;
+                        positions.Add(t);
+
+                        if (spr.bounds.size.x < tileSize)
+                        {
+                            tileSize = Mathf.Round(spr.bounds.size.x * 100) / 100;
+                        }
+                    }
+                }
+            }
+            TerrainData tData = new TerrainData();
+            tData.heightmapResolution = terrainResolution;
+            tData.size = new Vector3(terrainResolution * tileSize, 1, terrainResolution * tileSize);
+
+            float[,] heights = new float[terrainResolution, terrainResolution];
+            foreach (var rect in positions)
+            {
+                int x = Mathf.RoundToInt(rect.x / tileSize) + terrainResolution / 2;
+                int y = Mathf.RoundToInt(rect.y / tileSize) + terrainResolution / 2;
+                int w = Mathf.RoundToInt(rect.width / tileSize);
+                int h = Mathf.RoundToInt(rect.height / tileSize);
+                //Debug.Log(x + ", " + y + ", " + w + ", " + h);
+
+                for (int xx = 0; xx < w; xx++)
+                {
+                    for (int yy = 0; yy < h; yy++)
+                    {
+                        heights[x+xx, y+yy] = 1;
+                    }
+                }
+
+                tData.SetHeights(0, 0, heights);
+            }
+
+            GameObject res = Terrain.CreateTerrainGameObject(tData);
+            res.transform.position = new Vector3(-(terrainResolution * tileSize) / 2, 1, -(terrainResolution * tileSize) / 2);
+            res.name = "NavMesh2D Terrain";
+            return res;
+        }
+
+        return null;
+    }
+}
---
+++ Assets/TestTerrain.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TestTerrain : MonoBehaviour {
+    private GameObject terrain;
+    private TerrainData terrainData;
+
+	// Use this for initialization
+	void Start () {
+        terrainData = new TerrainData();
+        float[,] heights = new float[10, 10];
+        for (int x = 0; x < 10; x++)
+        {
+            for (int y = 0; y < 10; y++)
+            {
+                heights[x, y] = 10;
+            }
+        }
+        terrainData.heightmapResolution = 66;
+        terrainData.SetHeights(0, 0, heights);
+        terrain = Terrain.CreateTerrainGameObject(terrainData);
+        terrain.transform.position = new Vector3(10, -10, 100);
+	}
+
+    // Update is called once per frame
+    void Update()
+    {
+	
+	}
+}
